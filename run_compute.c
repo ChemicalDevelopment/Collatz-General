5fc071a893c49ce28b4e7b94695d06b97081e062
Cade Brown <brown.cade@gmail.com>
2016-10-31 11:22:59 -0400

    Working

--- run_compute.c
+++ run_compute.c
@@ -2,7 +2,7 @@
 
 Run like
 
-./run_compute.o <residue class> <mul . . . > <add . . . > <div . . .> 
+./run_compute.o <max> <max_trials> <residue class> <mul . . . > <add . . . > <div . . .> 
 
 
 */
@@ -10,8 +10,8 @@ Run like
 #include <stdio.h>
 #include <stdlib.h>
 
-int MAX = 100;
-int MAX_TRIALS = 100;
+int MAX;
+int MAX_TRIALS;
 
 int residue_class;
 
@@ -28,6 +28,8 @@ int transform_num(int x) {
 
 int main(int argc, char *argv[]) {
     int ci = 0;
+    MAX = strtol(argv[++ci], NULL, 10);
+    MAX_TRIALS = strtol(argv[++ci], NULL, 10);
     residue_class = strtol(argv[++ci], NULL, 10);
     residue_mul = (int *)malloc(sizeof(int) * residue_class);   
     residue_add = (int *)malloc(sizeof(int) * residue_class);   
@@ -38,7 +40,23 @@ int main(int argc, char *argv[]) {
     for (i = 0; i < residue_class; ++i) residue_add[i] = strtol(argv[++ci], NULL, 10);
     for (i = 0; i < residue_class; ++i) residue_div[i] = strtol(argv[++ci], NULL, 10);
 
-    int x, r_x, trials, repeats;
+    printf("Running CollatzL v0.0.1\n");
+    printf("Testing from 0 to %d, going up to %d max trials\n", MAX, MAX_TRIALS);
+    printf("\n");
+    for (i = 0; i < residue_class; ++i) {
+        printf("If x %% %d = %d, then f(x) = ", residue_class, i);
+        if (residue_div[i] != 1 && residue_add[i] != 0) printf("(");
+        if (residue_mul[i] != 1) printf("%d", residue_mul[i]);
+        printf("x");
+        if (residue_add[i] != 0) printf("+%d", residue_add[i]);
+        if (residue_div[i] != 1 && residue_add[i] != 0) printf(")");
+        if (residue_div[i] != 1) printf("/%d", residue_div[i]);
+        printf("\n");
+    }   
+
+    printf("Does the iteration of f(f(f(f(f...f(x))))) eventualy follow a pattern?\n");
+
+    int x, r_x, trials, repeats, all_repeated = 1;
     for (x = 0; x < MAX; ++x) {
         trials = 0;
         r_x = x;
@@ -54,9 +72,17 @@ int main(int argc, char *argv[]) {
             trials += 1;
         }
         if (repeats == 1) {
-            printf("%d repeats\n", x);
+            //printf("%d repeats\n", x);
+        } else {
+            printf("%d does not repeat\n", x);
         }
+        all_repeated = all_repeated & repeats;
     }
+
+    if (all_repeated == 1) {
+        printf("All tested inputs repeat\n");
+    }
+
     free(residue_add);
     free(residue_mul);
     free(residue_div);
